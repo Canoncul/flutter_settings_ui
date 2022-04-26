eeb62e37191bfdd7d4019ffa0d7a29917cc1d426
Can Öncül <canoncul@msn.com>
2022-04-27 02:02:20 +0300

    Ios SubTitleWidget Fix

--- lib/src/cupertino_settings_item.dart
+++ lib/src/cupertino_settings_item.dart
@@ -106,30 +106,23 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
       dense: widget.listTileTheme?.dense ?? listTileTheme.dense,
       shape: widget.listTileTheme?.shape ?? listTileTheme.shape,
       style: widget.listTileTheme?.style ?? listTileTheme.style,
-      selectedColor:
-          widget.listTileTheme?.selectedColor ?? listTileTheme.selectedColor,
+      selectedColor: widget.listTileTheme?.selectedColor ?? listTileTheme.selectedColor,
       iconColor: widget.listTileTheme?.iconColor ?? listTileTheme.iconColor,
       textColor: widget.listTileTheme?.textColor ?? listTileTheme.textColor,
-      contentPadding:
-          widget.listTileTheme?.contentPadding ?? listTileTheme.contentPadding,
+      contentPadding: widget.listTileTheme?.contentPadding ?? listTileTheme.contentPadding,
       tileColor: widget.listTileTheme?.tileColor ?? listTileTheme.tileColor,
-      selectedTileColor: widget.listTileTheme?.selectedTileColor ??
-          listTileTheme.selectedTileColor,
-      enableFeedback:
-          widget.listTileTheme?.enableFeedback ?? listTileTheme.enableFeedback,
-      horizontalTitleGap: widget.listTileTheme?.horizontalTitleGap ??
-          listTileTheme.horizontalTitleGap,
-      minVerticalPadding: widget.listTileTheme?.minVerticalPadding ??
-          listTileTheme.minVerticalPadding,
-      minLeadingWidth: widget.listTileTheme?.minLeadingWidth ??
-          listTileTheme.minLeadingWidth,
+      selectedTileColor: widget.listTileTheme?.selectedTileColor ?? listTileTheme.selectedTileColor,
+      enableFeedback: widget.listTileTheme?.enableFeedback ?? listTileTheme.enableFeedback,
+      horizontalTitleGap:
+          widget.listTileTheme?.horizontalTitleGap ?? listTileTheme.horizontalTitleGap,
+      minVerticalPadding:
+          widget.listTileTheme?.minVerticalPadding ?? listTileTheme.minVerticalPadding,
+      minLeadingWidth: widget.listTileTheme?.minLeadingWidth ?? listTileTheme.minLeadingWidth,
       child: const SizedBox.shrink(),
     );
 
     final iconThemeData = IconThemeData(
-      color: widget.enabled
-          ? _iconColor(theme, tileTheme)
-          : CupertinoColors.inactiveGray,
+      color: widget.enabled ? _iconColor(theme, tileTheme) : CupertinoColors.inactiveGray,
     );
 
     Widget? leadingIcon;
@@ -154,17 +147,24 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
 
     final Widget titleSection;
     if (widget.subtitle == null) {
-      titleSection = widget.labelWidget ??
-          Text(
-            widget.label ?? '',
-            overflow: TextOverflow.ellipsis,
-            style: widget.labelTextStyle ??
-                TextStyle(
-                    fontSize: 16,
-                    color: !widget.enabled
-                        ? CupertinoColors.inactiveGray
-                        : widget.listTileTheme?.textColor),
-          );
+      titleSection = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          widget.labelWidget ??
+              Text(
+                widget.label ?? '',
+                overflow: TextOverflow.ellipsis,
+                style: widget.labelTextStyle ??
+                    TextStyle(
+                        fontSize: 16,
+                        color: !widget.enabled
+                            ? CupertinoColors.inactiveGray
+                            : widget.listTileTheme?.textColor),
+              ),
+          widget.subtitleWidget != null ? widget.subtitleWidget! : Container(),
+        ],
+      );
     } else {
       titleSection = Column(
         mainAxisSize: MainAxisSize.min,
@@ -288,8 +288,7 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: () {
-        if ((widget.onPress != null || widget.onToggle != null) &&
-            widget.enabled) {
+        if ((widget.onPress != null || widget.onToggle != null) && widget.enabled) {
           if (mounted) {
             setState(() {
               pressed = true;
@@ -344,9 +343,7 @@ class CupertinoSettingsItemState extends State<CupertinoSettingsItem> {
           borderRadius: radius(isLargeScreen),
           color: tileTheme.tileColor ?? calculateBackgroundColor(context),
         ),
-        height: widget.subtitle == null && widget.subtitleWidget == null
-            ? 44.0
-            : 57.0,
+        height: widget.subtitle == null && widget.subtitleWidget == null ? 44.0 : 66.0,
         child: Row(
           children: rowChildren,
         ),
